c6a4785e3d7cb495d2970839ea674b14d2b1b1b4
Hitasri <your-email@gmail.com>
2026-06-24 18:47:02 +0530

    Fixed CSS filename case

--- ptw-frontend/src/pages/Register.jsx
+++ ptw-frontend/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import "../styles/Login.css";
+import "../styles/login.css";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import API from "../services/api";
